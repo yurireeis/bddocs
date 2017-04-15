61bc47f32f8de5713f1c6aecedb2fe3e5f65223d
Yuri Reis <yuri.reis@msn.com>
2017-04-15 12:09:02 -0300

    HTML class is already integrated with Documentation pattern

--- src/html/html.py
+++ src/html/html.py
@@ -3,11 +3,12 @@ from src.markup import markup
 
 class HTML(object):
     def __init__(self, documentation):
-        self.documentation = documentation
+        self.bdd = documentation
         self.page = markup.page()
         self.set_up()
-        self.unordered_list()
-        self.paragraphs()
+        # self.unordered_list()
+        # self.paragraphs()
+        self.set_content()
 
     def set_up(self):
         self.page.init(title="My title",
@@ -31,6 +32,46 @@ class HTML(object):
         """
         images = ("thumb1.jpg", "thumb2.jpg", "more.jpg", "more2.jpg")
         self.page.img(src=images, width=100, height=80, alt="Thumbnails")
+
+    def set_content(self):
+        for artifact in self.bdd.artifacts:
+            feature = artifact.feature
+
+            # feature title
+            self.page.p(feature.title)
+
+            if feature.actor:
+                self.page.p(feature.actor)
+            if feature.objective:
+                self.page.p(feature.objective)
+            if feature.value:
+                self.page.p(feature.value)
+
+            if not feature.actor and not feature.objective and not feature.value:
+                self.page.p('No value proposition found')
+
+            # break line
+            self.page.br()
+
+            if feature.scenarios:
+
+                for scenario in feature.scenarios:
+                    self.page.p(scenario.title)
+
+                    if not scenario.steps:
+                        self.page.p('[This scenario dont have steps yet]')
+                    else:
+                        for step in scenario.steps:
+                            self.page.p(step.title)
+
+                # break line after scenario
+                self.page.br()
+
+            else:
+                self.page.p('This feature Dont have scenarios')
+
+                # break line if you dont have scenarios in this feature
+                self.page.br()
 
     def preview(self):
         return self.page

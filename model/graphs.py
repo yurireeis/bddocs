60f4fc4432f6949341169370ee5a8a178d849076
Yuri Reis <yuri.reis@msn.com>
2017-04-17 17:54:35 -0300

    Generate example graphs: done!

--- model/graphs.py
+++ model/graphs.py
@@ -1,21 +1,68 @@
 import plotly.tools as config
 import plotly.plotly as py
-from plotly.graph_objs import *
+import plotly.graph_objs as go
 
-username = 'yurireis'
+username = 'yurireeis'
 credential = 'JS2YQg2NQ7WP1n2OZc8i'
 
 
 class Graphs(object):
-    def __init__(self, user, api_key, readable=False, privacy='private'):
+    def __init__(self, user, api_key, readable=True, privacy='public'):
         """
 
+        :param document:
+        :type document: Documentation
         :param readable:
         :param privacy:
         """
+        # self.document = document
         config.set_credentials_file(username=user, api_key=api_key)
         config.set_config_file(world_readable=readable, sharing=privacy)
+        self.ids = self.get_stream_ids()
 
-    def get
+    def get_stream_ids(self):
+        """
 
-Graphs(username, credential)
+        :return: list of ids
+        :rtype: str[]
+        """
+        return config.get_credentials_file()['stream_ids']
+
+    def general_coverage(self, title=None):
+        """
+
+        :return:
+        """
+        trace0 = go.Scatter(
+            x=[1, 2, 3, 4],
+            y=[10, 15, 13, 17]
+        )
+        trace1 = go.Scatter(
+            x=[1, 2, 3, 4],
+            y=[16, 5, 11, 9]
+        )
+        data = go.Data([trace0, trace1])
+
+        if title:
+            layout = go.Layout(title=title)
+            fig = go.Figure(data=data, layout=layout)
+            return py.plot(fig, filename='python-streaming')
+
+        return py.plot(data, filename='python-streaming')
+
+    def stream(self, pos):
+        """
+
+        :param pos:
+        :param max_points:
+        :return:
+        """
+        return go.Stream(
+            token=self.ids[pos],
+        )
+
+
+test = Graphs(username, credential)
+test.stream(0)
+test.general_coverage('Novo título')
+pass
